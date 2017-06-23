db803bed0c11ccf2b4e5e41482535f9a7a16553e
SErAphLi <seraphlivery@gmail.com>
2017-06-23 15:36:11 +0800

    更新代码添加提示

--- wic.py
+++ wic.py
@@ -1,52 +1,64 @@
-# wechat image convert
-import binascii
+import traceback
 
-# first need two file to find rule
-trans = 'color_trans.dat'
-sheet = 'color_sheet.jpg'
+try:
+    # wechat image convert
+    import binascii
 
-binfile = open(trans,'rb')
-a = binfile.read()
-hex_trans = binascii.b2a_hex(a)
+    # first need two file to find rule
+    trans = 'color_trans.dat'
+    sheet = 'color_sheet.jpg'
 
-binfile = open(sheet,'rb')
-a = binfile.read()
-hex_sheet = binascii.b2a_hex(a)
+    binfile = open(trans, 'rb')
+    a = binfile.read()
+    hex_trans = binascii.b2a_hex(a)
 
-rule = [0] * 256
-flag = [False] * 256
-for i in range(len(hex_trans)/2):
-  hex_t = hex_trans[2*i:2*(i+1)]
-  hex_s = hex_sheet[2*i:2*(i+1)]
-  int_t = int(hex_t,16)
-  int_s = int(hex_s,16)
-  if flag[int_t] == False:
-    rule[int_t] = int_s
-    flag[int_t] = True
-  if flag.count(True) == 256:
-    break
+    binfile = open(sheet, 'rb')
+    a = binfile.read()
+    hex_sheet = binascii.b2a_hex(a)
 
-# second convert dat file
-in_file = 'in.dat'
-out_file = 'out.jpg'
+    rule = [0] * 256
+    flag = [False] * 256
+    for i in range(len(hex_trans) // 2):
+        hex_t = hex_trans[2 * i:2 * (i + 1)]
+        hex_s = hex_sheet[2 * i:2 * (i + 1)]
+        int_t = int(hex_t, 16)
+        int_s = int(hex_s, 16)
+        if flag[int_t] == False:
+            rule[int_t] = int_s
+            flag[int_t] = True
+        if flag.count(True) == 256:
+            break
 
-binfile = open(in_file, 'rb')
-a = binfile.read()
-binfile.close()
-hex_in = binascii.b2a_hex(a)
+    # second convert dat file
+    in_file = 'in.dat'
+    out_file = 'out.jpg'
 
-out_ret = ''
-for i in range(len(hex_in)/2):
-  hex_t = hex_in[2*i:2*(i+1)]
-  int_t = int(hex_t,16)
-  int_ret = rule[int_t] 
-  if int_ret>15:
-    hex_ret = hex(int_ret)[2:]
-  else:
-    hex_ret = '0' + hex(int_ret)[2:]
-  out_ret += hex_ret
-hex_file = out_ret.decode('hex')
+    binfile = open(in_file, 'rb')
+    a = binfile.read()
+    binfile.close()
+    hex_in = binascii.b2a_hex(a)
 
-binfile = open(out_file, 'wb')
-binfile.write(hex_file)
-binfile.close()
+    out_ret = ''
+    for i in range(len(hex_in) // 2):
+        hex_t = hex_in[2 * i:2 * (i + 1)]
+        int_t = int(hex_t, 16)
+        int_ret = rule[int_t]
+        if int_ret > 15:
+            hex_ret = hex(int_ret)[2:]
+        else:
+            hex_ret = '0' + hex(int_ret)[2:]
+        out_ret += hex_ret
+    hex_file = bytes.fromhex(out_ret)
+
+    binfile = open(out_file, 'wb')
+    binfile.write(hex_file)
+    binfile.close()
+    print(u'转换成功')
+except Exception as e:
+    print('Exception Info')
+    print('-' * 10)
+    print('traceback.format_exc():')
+    print(traceback.format_exc())
+    print('-' * 10)
+
+input(u'按任意键退出\n')
